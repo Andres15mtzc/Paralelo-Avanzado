2b5e0ab5902ba0df0b489be0fdc4d0018cb0a74e
LOLImaze <andresmtzc10@hotmail.com>
2024-04-26 21:54:04 -0600

    Partial 2

---
+++ Partial 2/Lab/lab04.cu
@@ -0,0 +1,59 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#define WIDTH 1024
+#define HEIGHT 1024
+
+using namespace std;
+
+void conv2D(int* mat, int* r, int w, int h) {
+    for (int row = 0; row < h; ++row) {
+        for (int col = 0; col < w; ++col) {
+            int suma = 0;
+            for (int i = -1; i <= 1; ++i) {
+                for (int j = -1; j <= 1; ++j) {
+                    int curRow = row + i;
+                    int curCol = col + j;
+                    if (curRow >= 0 && curRow < h && curCol >= 0 && curCol < w) {
+                        suma += mat[curRow * w + curCol];
+                    }
+                }
+            }
+            r[row * w + col] = suma;
+        }
+    }
+}
+
+int main() {
+    const int size = WIDTH * HEIGHT;
+    int* M_bef = new int[size];
+    int* M_conv = new int[size];
+
+    srand(time(NULL));
+    for (int i = 0; i < size; ++i) {
+        M_bef[i] = rand() % 9;
+    }
+
+    cout << "M_before: \n";
+    for (int i = 0; i < 20; ++i) {
+        cout << "M_bef[" << i << "] = " << M_bef[i] << endl;
+    }
+
+    conv2D(M_bef, M_conv, WIDTH, HEIGHT);
+
+    cout << "M_convolution: \n";
+    for (int i = 0; i < 20; ++i) {
+        cout << "M_conv[" << i << "] = " << M_conv[i] << endl;
+    }
+
+    delete[] M_bef;
+    delete[] M_conv;
+
+    return 0;
+}
---
+++ Partial 2/Lab/lab05.cu
@@ -0,0 +1,54 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#define WIDTH 1024
+#define HEIGHT 1024
+
+using namespace std;
+
+void transpose(int* in, int* out, int w, int h) {
+    for (int x = 0; x < w; ++x) {
+        for (int y = 0; y < h; ++y) {
+            int i_in = y * w + x;
+            int i_out = x * h + y;
+            out[i_out] = in[i_in];
+        }
+    }
+}
+
+int main() {
+    const int size = WIDTH * HEIGHT;
+
+    int* M = new int[size];
+    int* T = new int[size];
+
+    // Initialize matrix with random values
+    srand(time(NULL));
+    for (int i = 0; i < size; ++i) {
+        M[i] = rand() % 9;
+    }
+
+    cout << "Original matrix: \n";
+    for (int i = 0; i < 20; ++i) {
+        cout << "M[" << i << "] = " << M[i] << endl;
+    }
+
+    transpose(M, T, WIDTH, HEIGHT);
+
+    cout << "Transposed matrix: \n";
+    for (int i = 0; i < 20; ++i) {
+        cout << "T[" << i << "] = " << T[i] << endl;
+    }
+
+    delete[] M;
+    delete[] T;
+
+    return 0;
+}
